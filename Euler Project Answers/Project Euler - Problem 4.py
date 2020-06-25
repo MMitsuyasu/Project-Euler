192e1c5e62e79d3cd0b713397ac1c336394a441d
MMitsuyasu <margaret.mitsuyasu@gmail.com>
2020-06-25 07:36:10 -0700

    Develop own official solution

--- Euler Project Answers/Project Euler - Problem 4.py
+++ Euler Project Answers/Project Euler - Problem 4.py
@@ -24,32 +24,87 @@
 ##
 ##    palStatus = match == palLength
 ##    return palStatus
+
+
+##def isPal(value):
+##    if str(value) == "".join(reversed(str(value))):
+##        return True
+##    else:
+##        return False
 ##
 ##
-##
-
-def isPal(value):
-    if str(value) == "".join(reversed(str(value))):
-        return True
-    else:
-        return False
+##w = 642
+##print(isPal(w))
+##p = 590095
+##print(isPal(p))
 
 
-x = "Hello"
-y = reversed(x)
-z = "".join(y)
-print(z)
 
-w = 642
-print(isPal(w))
-p = 590095
-print(isPal(p))
+## Generate palindrome first and check for factors
+# Products of two 3-digit factors:
+#    100 x 100 =  10,000
+#    999 x 999 = 998,001
+# Need to test palindromes between 10000 and 998001
+
+
 
 ############################################################################
 ############################################################################
 
 ## Official Solution Used
 
+found = False
+
+# Generate and test 6-digit palindromes
+for a in range(9, 0, -1):
+    for b in range(9, -1, -1):
+        for c in range(9, -1, -1):
+            # Set largest palindrome under 998001 (max product)
+            if a == 9 and b == 9 and c < 8: continue
+            
+            prep = [str(a), str(b), str(c), str(c), str(b), str(a)]
+            pal = int("".join(prep))
+            
+            # Check if palindrome is product of two 3-digit factors
+            x = 999
+            while x >= 100:
+                if pal % x == 0 and len(str(int(pal / x))) == 3:
+                    found = True
+                    break
+                x = x - 1
+                
+            # Stop generating palindromes once answer is identified
+            if found: break
+        if found: break
+    if found: break
+
+
+# Generate and test 5-digit palindromes
+# (only if answer has not been identified)
+if found == False:
+    for a in range(9, 0, -1):
+        for b in range(9, -1, -1):
+            for c in range(9, -1, -1):
+                
+                prep = [str(a), str(b), str(c), str(b), str(a)]
+                pal = int("".join(prep))
+                
+                # Check if palindrome is product of two 3-digit factors
+                x = 999
+                while x >= 100:
+                    if pal % x == 0 and len(str(int(pal / x))) == 3:
+                        found = True
+                        break
+                    x = x - 1
+                    
+                # Stop generating palindromes once answer is identified
+                if found: break
+            if found: break
+        if found: break
+
+
+print(f"The largest palindromic product of two 3-digit factors is {pal}." \
+      f"\nIt is the product of {x} and {int(pal / x)}.")
 
 
 ############################################################################
