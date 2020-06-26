49a327469ee69e50a28bd704d46b060fb33dce25
MMitsuyasu <margaret.mitsuyasu@gmail.com>
2020-06-25 18:56:28 -0700

    Create function to find prime factors

--- Euler Project Answers/Project Euler - Problem 5.py
+++ Euler Project Answers/Project Euler - Problem 5.py
@@ -18,6 +18,38 @@
 # Multiply all identified factors together
 
 
+# Identify prime numbers <= target
+def primeFactors(target):
+    import math
+    workingTarget = target
+    primeList = [1]
+
+    # Checks for 2 as a factor
+    if workingTarget % 2 == 0:
+        primeList.append(2)
+        while workingTarget % 2 == 0:
+            workingTarget = workingTarget / 2
+
+    # Checks for other prime factors
+    maxFactor = math.sqrt(workingTarget)
+    factor = 3
+
+    while workingTarget > 1 and factor <= maxFactor:
+        if workingTarget % factor == 0:
+            primeList.append(factor)
+            while workingTarget % factor == 0:
+                workingTarget = workingTarget / factor
+            maxFactor = math.sqrt(workingTarget)
+        factor = factor + 2
+
+    primeList.append(int(workingTarget))
+
+    return primeList
+
+print(primeFactors(688500))
+
+
+
 ############################################################################
 ############################################################################
 
